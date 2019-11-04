ffb0d9247c90827c816269f112213a9279a3a321
khoi <khoiracle@gmail.com>
2019-11-04 23:50:03 +0700

    Remove Effects

--- pomo/Vuex.swift
+++ pomo/Vuex.swift
@@ -16,28 +16,6 @@ protocol Action {
   func mapToMutation() -> AnyPublisher<Mutation, Never>
 }
 
-public struct Effect<A> {
-  public let run: (@escaping (A) -> Void) -> Void
-  
-  public init(run: @escaping (@escaping (A) -> Void) -> Void) {
-    self.run = run
-  }
-  
-  public func map<B>(_ f: @escaping (A) -> B) -> Effect<B> {
-    return Effect<B> { callback in self.run { a in callback(f(a)) } }
-  }
-  
-  public func receive(on queue: DispatchQueue) -> Effect {
-    return Effect { callback in
-      self.run { a in
-        queue.async {
-          callback(a)
-        }
-      }
-    }
-  }
-}
-
 final class Store<State, Mutation>: ObservableObject {
   @Published public private(set) var state: State
   
