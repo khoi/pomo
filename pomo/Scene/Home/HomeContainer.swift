12f9364417183234514af89f4eeac41bf01a3a9f
khoi <khoiracle@gmail.com>
2019-11-05 14:42:35 +0700

    Icon

--- pomo/Scene/Home/HomeContainer.swift
+++ pomo/Scene/Home/HomeContainer.swift
@@ -10,30 +10,26 @@ import SwiftUI
 
 struct HomeContainer: View {
   @EnvironmentObject var store: Store<AppState, AppMutation>
-    
+
   @State var timeLeft: TimeInterval = 0
-  
+
   private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-  
+
   var body: some View {
-    VStack {
+    VStack(spacing: 16) {
       ZStack {
         ActivityRing(progress: ringProgress(timeLeft: self.timeLeft, duration: self.store.state.defaultDuration))
         Text(format(duration: self.timeLeft))
           .font(.largeTitle)
-  
       }
       .padding(16)
-      
-      if self.store.state.started == nil {
-        Button("Start") {
-          self.store.dispatch(AppAction.startTimer)
-        }
-      }
-      else {
-        Button("Stop") {
-          self.store.dispatch(AppAction.stopTimer)
-        }
+
+      Button(action: {
+        self.store.dispatch(self.timerStarted ? AppAction.stopTimer : AppAction.startTimer)
+      }) {
+        Image(systemName: self.timerStarted ? "pause.circle" : "play.circle")
+          .font(.largeTitle)
+          .foregroundColor(.pink)
       }
     }
     .onReceive(timer) { _ in
@@ -48,14 +44,17 @@ struct HomeContainer: View {
       }
       self.timeLeft = timeLeft
     }
-  
   }
-  
+
+  var timerStarted: Bool {
+    store.state.started != nil
+  }
+
   func ringProgress(timeLeft: TimeInterval, duration: TimeInterval) -> CGFloat {
     let progress = timeLeft / duration
     return CGFloat(max(0, min(1, progress)))
   }
-  
+
   func format(duration: TimeInterval) -> String {
     let formatter = DateComponentsFormatter()
     formatter.allowedUnits = [.minute, .second]
@@ -65,7 +64,6 @@ struct HomeContainer: View {
     return formatter.string(from: duration)!
   }
 }
-
 
 struct HomeContainerView_Previews: PreviewProvider {
   static var previews: some View {
