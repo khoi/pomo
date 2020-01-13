30fd6897e0e40aa17519be8a1fd942c49bfdad51
Danh Dang <danh.dang@zalora.com>
2020-01-13 22:54:48 +0700

    Fix compile error

--- pomo/AppReducer.swift
+++ pomo/AppReducer.swift
@@ -9,51 +9,51 @@
 import Foundation
 
 struct AppState {
-  var timer = TimerState()
-  var statistic = StatisticState()
+    var timer = TimerState()
+    var statistic = StatisticState()
 }
 
 enum AppAction {
-  case timer(TimerAction)
-  case statistic(StatisticAction)
-  case settings(SettingsAction)
-  
-  var timer: TimerAction? {
-    get {
-      guard case let .timer(value) = self else { return nil }
-      return value
-    }
-    set {
-      guard case .timer = self, let newValue = newValue else { return }
-      self = .timer(newValue)
-    }
-  }
-  
-  var statistic: StatisticAction? {
-    get {
-      guard case let .statistic(value) = self else { return nil }
-      return value
-    }
-    set {
-      guard case .statistic = self, let newValue = newValue else { return }
-      self = .statistic(newValue)
-    }
-  }
-}
+    case timer(TimerAction)
+    case statistic(StatisticAction)
+    case settings(SettingsAction)
 
-var settings: SettingsAction? {
-  get {
-    guard case let .settings(value) = self else { return nil }
-    return value
-  }
-  set {
-    guard case .settings = self, let newValue = newValue else { return }
-    self = .settings(newValue)
-  }
+    var timer: TimerAction? {
+        get {
+            guard case let .timer(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .timer = self, let newValue = newValue else { return }
+            self = .timer(newValue)
+        }
+    }
+
+    var statistic: StatisticAction? {
+        get {
+            guard case let .statistic(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .statistic = self, let newValue = newValue else { return }
+            self = .statistic(newValue)
+        }
+    }
+
+    var settings: SettingsAction? {
+        get {
+            guard case let .settings(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .settings = self, let newValue = newValue else { return }
+            self = .settings(newValue)
+        }
+    }
 }
 
 let appReducer = combine(
-  pullback(timerReducer, value: \AppState.timer, action: \AppAction.timer),
-  pullback(statisticReducer, value: \AppState.statistic, action: \AppAction.statistic),
-  pullback(settingsReducer, value: \AppState.timer.timerSettings, action: \AppAction.settings)
+    pullback(timerReducer, value: \AppState.timer, action: \AppAction.timer),
+    pullback(statisticReducer, value: \AppState.statistic, action: \AppAction.statistic),
+    pullback(settingsReducer, value: \AppState.timer.timerSettings, action: \AppAction.settings)
 )
